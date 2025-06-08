520881bc9ac2cdf41b28e9e711c24b64a7230038
sumanyu sharma <sumanyu198@gmail.com>
2025-06-09 04:05:19 +0530

    updated bakcend url in axiosInstance

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-    baseURL: process.env.BACKEND_URL,
+    baseURL: import.meta.env.BACKEND_URL,
     withCredentials: true // to send cookies in every single request
 });
