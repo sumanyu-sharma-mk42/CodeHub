1799b890365254d6e401e69c034dd54f15d11f94
sumanyu sharma <sumanyu198@gmail.com>
2025-06-09 03:58:53 +0530

    added bakcend url in axiosInstance

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-    baseURL: "http://localhost:3000/api",
+    baseURL: process.env.BACKEND_URL,
     withCredentials: true // to send cookies in every single request
 });
