c52e4cbe21afca6d393dd77d4efd707fab2f5998
sumanyu sharma <sumanyu198@gmail.com>
2025-06-09 03:34:47 +0530

    modified

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,7 +7,7 @@ import { app } from './utils/socket.js';
 
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: process.env.FRONTEND_URL,
     credentials: true
 }));
 app.use(express.json({ limit: "10mb" }));
